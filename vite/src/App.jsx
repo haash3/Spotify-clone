3208508288861fc9c3d1dae08e88ac4946842999
Harshitha-0306 <harshitha.y2022@gmail.com>
2024-02-13 22:15:57 +0530

    Create Login Page

--- vite/src/App.jsx
+++ vite/src/App.jsx
@@ -1,12 +1,16 @@
 import { useState } from 'react'
 import './App.css'
+import Login from './login'
 
 function App() {
-  const [count, setCount] = useState(0)
+  // const [count, setCount] = useState(0)
 
   return (
     <>
-      
+
+      {/* Login Component */}
+      <Login/>
+
     </>
   )
 }
---
+++ vite/src/login.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import './login.css';
+// Imports END
+
+
+
+
+
+
+function Login(){
+
+    // RETURN statement
+    return(
+        <>
+        <div className="login">
+      {/* Spotify Logo */}
+        <img src='https://getheavy.com/wp-content/uploads/2019/12/spotify2019-830x350.jpg'/>
+        {/* LogIn Button */}
+        <a> Login With Spotify</a>
+        </div>
+        
+        </>
+    )
+}
+
+
+export default Login
